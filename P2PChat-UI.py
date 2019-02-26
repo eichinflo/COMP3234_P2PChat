b3a0d16b539503b1f543a0f34c12b9643f376716
Florian Eichin <eichinflo@aol.com>
2019-02-26 18:19:46 +0800

    Reformatted existing P2PChat-UI.py to go along with PEP8 styleguide and added docstrings.

--- P2PChat-UI.py
+++ P2PChat-UI.py
@@ -16,21 +16,20 @@ import socket
 #
 
 
-
 #
 # This is the hash function for generating a unique
 # Hash ID for each peer.
 # Source: http://www.cse.yorku.ca/~oz/hash.html
 #
-# Concatenate the peer's username, str(IP address), 
-# and str(Port) to form a string that be the input 
+# Concatenate the peer's username, str(IP address),
+# and str(Port) to form a string that be the input
 # to this hash function
 #
 def sdbm_hash(instr):
-	hash = 0
-	for c in instr:
-		hash = int(ord(c)) + (hash << 6) + (hash << 16) - hash
-	return hash & 0xffffffffffffffff
+    hash = 0
+    for c in instr:
+        hash = int(ord(c)) + (hash << 6) + (hash << 16) - hash
+    return hash & 0xffffffffffffffff
 
 
 #
@@ -38,30 +37,102 @@ def sdbm_hash(instr):
 #
 
 def do_User():
-	outstr = "\n[User] username: "+userentry.get()
-	CmdWin.insert(1.0, outstr)
-	userentry.delete(0, END)
+    """
+    This function implements the [User]-button:
+    Before joining any chatroom groups the end-user must register with
+    the P2PChat program his/her nickname (username), which will appear
+    in all messages sent by the end-user to the chatroom group as well as
+    be appeared in the member list of that chatroom group stored in the
+    Room server. The end-user can rename his/her username by using this
+    button only before joining any chatroom group. After the end-user has
+    registered his/her username, the program prints a message in the Command
+    Window.
+    Usernames consist of a single word with a length of at most 32 printable
+    ASCII characters (excluding the ‘:’ character which is being used as the
+    sentinel symbol in our prototol).
+    """
+    outstr = "\n[User] username: " + userentry.get()
+    CmdWin.insert(1.0, outstr)
+    userentry.delete(0, END)
 
 
 def do_List():
-	CmdWin.insert(1.0, "\nPress List")
+    """
+    This function implements the [List]-button:
+    To get the list of chatroom groups registered in the Room server by
+    sending a LIST request to the Room server. After receiving the list of
+    chatroom groups from the Room server, the program outputs the chatroom
+    names to the Command Window.
+    """
+    CmdWin.insert(1.0, "\nPress List")
 
 
 def do_Join():
-	CmdWin.insert(1.0, "\nPress JOIN")
+    """
+    This function implements the [Join]-button:
+    To join a target chatroom group. The end-user must provide the target
+    chatroom name via the user input field; otherwise, the program
+    displays an error message in the Command Window. If the P2PChat
+    program has already joined a chatroom group, the system should reject
+    this request and display an error message in the Command Window. After
+    getting the chatroom name, the P2PChat program sends a JOIN request to
+    the Room server, and if succeeded, the Room server should send back
+    the list of members in that chatroom together with their contact info.
+    The member list should include this newly joined P2PChat program.
+    Once, the P2PChat program gets the member list, it tries to join the
+    chatroom network by initiating a TCP connection to one of the
+    chatroom’s members. Once successfully linked to a member, this P2PChat
+    program is considered as successfully CONNECTED to the chatroom network
+    and it reports the status to the end-user via the Command Window. More
+    details on the interactions between the P2PChat program, the Room
+    server, and another peer will be covered in the communication protocol
+    section.
+    """
+    CmdWin.insert(1.0, "\nPress JOIN")
 
 
 def do_Send():
-	CmdWin.insert(1.0, "\nPress Send")
+    """
+    This function implements the [Send]-button:
+    To send a message to the chatroom network, i.e., to everyone in the
+    chatroom. The P2PChat program must be CONNECTED to the chatroom network
+    before sending a message. The message is sent to the chatroom network
+    via all peers that are currently connected (forward link and backward
+    links) to this P2PChat program. The program also displays the message to
+    the Message Window together with the username to identify who gave out
+    this message. You may assume the length of the text content is less than
+    500 bytes, but it is possible that the ‘:’ character may appear as text
+    content in the message.
+    """
+    CmdWin.insert(1.0, "\nPress Send")
 
 
 def do_Poke():
-	CmdWin.insert(1.0, "\nPress Poke")
+    """
+    This function implements the [Poke]-button:
+    to give a ‘Poke’ to a member in the chatroom. The P2PChat program must
+    have JOINED the chatroom before sending a Poke to a member in the
+    chatroom. The end-user must provide the target member’s nickname via the
+    user input field; otherwise, the program displays “To whom you want to
+    send the poke?” and the list of nicknames in this chatroom to the
+    Command Window. Given the member’s nickname, the program sends a UDP
+    message directly to that member’s UDP socket and an acknowledgment
+    response is expected from the target's P2PChat program. In addition,
+    a Poke message will be displayed in the target’s Message Window (as well
+    as the Command Window). More details on the interactions will be covered
+    in the communication protocol section.
+    """
+    CmdWin.insert(1.0, "\nPress Poke")
 
 
 def do_Quit():
-	CmdWin.insert(1.0, "\nPress Quit")
-	sys.exit(0)
+    """
+    This function implements the [Quit]-button:
+    to exit from the program. Before termination, the P2PChat program closes
+    all TCP connections and releases all resources.
+    """
+    CmdWin.insert(1.0, "\nPress Quit")
+    sys.exit(0)
 
 
 #
@@ -70,7 +141,7 @@ def do_Quit():
 win = Tk()
 win.title("MyP2PChat")
 
-#Top Frame for Message display
+# Top Frame for Message display
 topframe = Frame(win, relief=RAISED, borderwidth=1)
 topframe.pack(fill=BOTH, expand=True)
 topscroll = Scrollbar(topframe)
@@ -112,13 +183,14 @@ bottscroll.pack(side=RIGHT, fill=Y, expand=True)
 CmdWin.config(yscrollcommand=bottscroll.set)
 bottscroll.config(command=CmdWin.yview)
 
-def main():
-	if len(sys.argv) != 4:
-		print("P2PChat.py <server address> <server port no.> <my port no.>")
-		sys.exit(2)
 
-	win.mainloop()
+def main():
+    if len(sys.argv) != 4:
+        print("P2PChat.py <server address> <server port no.> <my port no.>")
+        sys.exit(2)
+
+    win.mainloop()
+
 
 if __name__ == "__main__":
-	main()
-
+    main()
