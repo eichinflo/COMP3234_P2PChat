854fd8773a45021d224cd140f7aa28c138693c20
Florian Eichin <eichinflo@aol.com>
2019-03-22 16:51:34 +0800

    Added do_User, do_List, do_Join functionality

--- P2PChat-UI.py
+++ P2PChat-UI.py
@@ -15,6 +15,14 @@ import socket
 # Global variables
 #
 
+USERNAME = None
+JOINED = False
+SERVER_ADDRESS = None
+SERVER_PORT = None
+MY_PORT = None
+MY_SOCKET = None
+CURRENT_CHATROOMS = None
+
 
 #
 # This is the hash function for generating a unique
@@ -51,9 +59,45 @@ def do_User():
     ASCII characters (excluding the ‘:’ character which is being used as the
     sentinel symbol in our prototol).
     """
-    outstr = "\n[User] username: " + userentry.get()
+    new_username = userentry.get()
+    outstr = do_User_(new_username) + '\n'
     CmdWin.insert(1.0, outstr)
     userentry.delete(0, END)
+
+
+def do_User_(username):
+    """
+    Just a helper function. We might reincorporate this code into do_User,
+    thought, that it would help testing and debugging to have it in this
+    function.
+
+    Here are the testcases for Stage one (doctest has a problem with
+    global variables right now, not sure how to make these tests pass)
+
+    >>> do_User_('')
+    'Invalid username: Cannot be empty'
+    >>> USERNAME == ''
+    False
+    >>> do_User_('spam')
+    'Succesfully changed username to: spam'
+    >>> USERNAME == 'spam'
+    True
+    >>> JOINED = True
+    >>> do_User_('ham')
+    'Could not change username: Already joined'
+    >>> USERNAME == 'spam'
+    True
+    """
+    global JOINED
+    # did client already join a server?
+    if JOINED is True:
+        return 'Could not change username: Already joined'
+    # do we have a valid username? We can include further checks here
+    if not username:
+        return 'Invalid username: Cannot be empty'
+    global USERNAME
+    USERNAME = username
+    return 'Succesfully changed username to: %s' % USERNAME
 
 
 def do_List():
@@ -64,7 +108,110 @@ def do_List():
     chatroom groups from the Room server, the program outputs the chatroom
     names to the Command Window.
     """
-    CmdWin.insert(1.0, "\nPress List")
+    outstr = "\nPress List\n" + do_List_()
+    CmdWin.insert(1.0, outstr)
+
+
+def do_List_():
+    """
+    Helper function for connecting to server and generating their user List.
+    """
+    if not MY_SOCKET:
+        # we must establish a new connection to server
+        error = connect_to_server()
+        if error:
+            return error
+
+    # send list request
+    print('[DEBUG] Sending LIST request.')
+    try:
+        MY_SOCKET.send(bytes('L::\r\n', 'ascii'))
+    except Exception as e:
+        print('[CLIENT_ERROR] Could not send LIST request:\n' + str(e))
+        return 'Error'
+
+    # their 1000 limits their number of names here :/
+    try:
+        response = MY_SOCKET.recv(1000).decode('ascii')
+    except Exception as e:
+        print('[CLIENT_ERROR] Could not receive list:\n' + str(e))
+        return 'Error'
+
+    # decode_list returns a list object with contents endswi boolean that is
+    # True iff we received an error message from the server
+    message, error_occured = decode_list(response)
+    if error_occured:
+        print('[SERVER_ERROR]: ' + message[0])
+        return 'Error'
+    else:
+        print('[DEBUG] Succesfully received list of names.')
+        global CURRENT_CHATROOMS
+        CURRENT_CHATROOMS = message
+        return '\n'.join(message)
+
+
+def connect_to_server():
+    """
+    Establish connection to chatroom server.
+    """
+    global SERVER_ADDRESS, SERVER_PORT, MY_PORT
+    print('[DEBUG] Establishing new connection to server.')
+    # create new_username socket, bind and connect
+    my_socket = socket.socket()
+    try:
+        my_socket.bind(('', MY_PORT))
+    except Exception as e:
+        my_socket.close()
+        print('[CLIENT_ERROR] Could not bind specified port:\n' + str(e))
+        return 'Error'
+
+    try:
+        my_socket.connect((SERVER_ADDRESS, SERVER_PORT))
+    except Exception as e:
+        my_socket.close()
+        print('[CLIENT_ERROR] Connection failed:\n' + str(e))
+        return 'Error'
+
+    global MY_SOCKET
+    MY_SOCKET = my_socket
+    return False
+
+
+def decode_list(response):
+    """
+    Helper function to decode server answers to LIST request.
+
+    Args:
+        response - string with LIST response message
+
+    Returns:
+        tuple of list of strings and boolean -
+            contents of the message divided into list-entries at ':'
+            and a value that indicates, if contents are errormsg or not
+
+    Examples:
+        >>> decode_list('G::\\r\\n')
+        ([], False)
+        >>> decode_list('G:HarryPotter:HermineGranger:RonWeasley::\\r\\n')
+        (['HarryPotter', 'HermineGranger', 'RonWeasley'], False)
+        >>> decode_list('F:HogwartsIsClosed::\\r\\n')
+        (['HogwartsIsClosed'], True)
+    """
+    if response.startswith('G:') and response.endswith(':\r\n'):
+        # valid response, retrieve names of chatrooms
+        names_str = response.strip('{G:|::\r\n}')
+        names_list = []
+        if names_str:
+            # message was not empty, we have chatroom names
+            names_list = names_str.split(':')
+        return (names_list, False)
+    elif response.startswith('F:') and response.endswith(':\r\n'):
+        # error response
+        return ([response.strip('{F:|::\r\n}')], True)
+    else:
+        # not a valid response, do nothing
+        print('[DEBUG] Did not receive a valid response from server: \n'
+              + response)
 
 
 def do_Join():
@@ -88,7 +235,72 @@ def do_Join():
     server, and another peer will be covered in the communication protocol
     section.
     """
-    CmdWin.insert(1.0, "\nPress JOIN")
+    chatroom = userentry.get()
+    outstr = "\n Press JOIN\n" + do_Join_(chatroom)
+    CmdWin.insert(1.0, outstr)
+    userentry.delete(0, END)
+
+
+def do_Join_(chatroom):
+    """
+    Helper function for do_Join function. TODO
+    """
+    print('[DEBUG] Attempting to join chatroom')
+    # check, if we are already in a chatroom or have a username
+    global JOINED
+    if JOINED:
+        print('[DEBUG] Already joined chatroom')
+        return 'You already joined a chatroom.'
+    global USERNAME
+    if not USERNAME:
+        print('[DEBUG] No username, aborting join')
+        return 'Specify username before joining chatrooms.'
+    # check if user gave us a chatroom name
+    if not chatroom:
+        print('[DEBUG] No chatroom name given')
+        return 'Specify name of the chatroom to join.'
+    # check, if we are already connected to a server
+    global MY_SOCKET
+    if not MY_SOCKET:
+        error = connect_to_server()
+        if error:
+            return error
+
+    # send JOIN request to server
+    my_address = MY_SOCKET.getsockname()
+    request = ("J:" + chatroom + ":" + USERNAME + ":" + my_address[0] + ":" +
+               str(my_address[1]) + "::\r\n")
+    try:
+        MY_SOCKET.send(bytes(request, 'ascii'))
+    except Exception as e:
+        print("[CLIENT_ERROR] Could not send JOIN request:\n" + str(e))
+        return 'Error'
+
+    try:
+        response = MY_SOCKET.recv(1000).decode('ascii')
+    except Exception as e:
+        print("[CLIENT_ERROR] Did nothing receive anything")
+        return 'Error'
+    if response.startswith('M:') and response.endswith(':\r\n'):
+        # valid response, we joined a chatroom
+        JOINED = True
+        print("[DEBUG] Joined a chatroom")
+        message = response.strip('{M:|::\r\n}').split(':')
+        msid = message[0]
+        users = [(name, address, int(port)) for
+                 name, address, port in
+                 zip(message[1::3], message[2::3], message[3::3])]
+        # TODO: implement rest of join functionality, make nice outputstring
+        return str(users)
+
+    elif response.startswith('F:') and response.endswith(':\r\n'):
+        # error message
+        return response.strip('{M:|::\r\n}')
+
+    else:
+        # not a valid response
+        print("[DEBUG] Did nothing receive valid response from server")
+        return 'Error'
 
 
 def do_Send():
@@ -188,6 +400,10 @@ def main():
     if len(sys.argv) != 4:
         print("P2PChat.py <server address> <server port no.> <my port no.>")
         sys.exit(2)
+    global SERVER_ADDRESS, SERVER_PORT, MY_PORT
+    SERVER_ADDRESS = sys.argv[1]
+    SERVER_PORT = int(sys.argv[2])
+    MY_PORT = int(sys.argv[3])
 
     win.mainloop()
 
